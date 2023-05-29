eab92a541431550294f2ba17833d021ea22561ec
NitinHirve <hirvernitin@gmail.com>
2023-05-29 21:08:23 +0530

    Fixed mobile navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,16 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link'
 import { useRouter } from "next/router";
 import PropTypes from 'prop-types';
 import { useEffect } from 'react'
 import MenuIcon from '@mui/icons-material/Menu';
 import styles from '../styles/bntnavbar.module.css';
+
+import { IoIosArrowForward } from 'react-icons/Io';
+import { IoIosArrowDown } from 'react-icons/Io';
+
+
+
 // import Menu from '@mui/material/Menu';
 // import MenuItem from '@mui/material/MenuItem';
 import {
@@ -35,6 +41,7 @@ function HideOnScroll(props) {
 function Navbar(props) {
 
     const router = useRouter();
+    const [pageRoute, setPageRoute] = useState('')
 
 
     useEffect(() => {
@@ -59,27 +66,143 @@ function Navbar(props) {
 
     const handleDrawerToggle = () => {
         setMobileOpen(!mobileOpen);
+        setPageRoute('')
     };
 
     const drawer = (
         <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
-            <Typography variant="h6" sx={{ my: 2 }}>
-                BNTSOFT
-            </Typography>
+            <Box
+                                variant="h6"
+                                component="div"
+                                sx={{textAlign: 'left', flexGrow: 1, color: 'text.primary', marginLeft: { xs: 'none', sm: '50px' }, padding: '10px', cursor: 'pointer' }}
+                            >
+                                {/* <p className="bntLogo" style={{fontFamily: 'Alexandria',fontWeight:'bold'}}> BNT-SOFT</p> */}
+
+                                <img src="https://bnt-soft.com/wp-content/uploads/2020/05/logoheader.png" alt="BnT Soft" style={{ height: "35px" }} />
+
+                            </Box>
             <Divider />
-            <List>
-                {navItems.map((item,i) => (
-                    <ListItem key={item} disablePadding>
-                        <ListItemButton sx={{ textAlign: 'center' }}>
-                        <Link href={item === 'Services' ? '' : navItemsNav[i]}>
-                                            
-                        <ListItemText primary={item} />
-                                         
-                                        </Link>
-                            
-                        </ListItemButton>
-                    </ListItem>
-                ))}
+            <List sx={{}}>
+                <>
+                    <Link href={'/'}>
+                        <ListItem key={'Home'} disablePadding>
+                            <ListItemButton className={` ${router.asPath == '/' ? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Home'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+
+                    <Link   onClick={(e) => {e.stopPropagation(); pageRoute==='/aboutus'? setPageRoute(''): setPageRoute('/aboutus') }} href={''}>
+                        <ListItem sx={{...(pageRoute ==='/aboutus' &&  {backgroundColor:'#dbdbd9'})}} key={'About Us'} disablePadding>
+                            <ListItemButton className={`${router.asPath.includes('/aboutus')? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'About Us'} /> { pageRoute ==='/aboutus' ? <IoIosArrowDown />: <IoIosArrowForward />}
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                    {pageRoute === '/aboutus' && <>
+                        <List className={styles.mobileNavbarDropdow}  sx={{backgroundColor:'#e3f2fc',padding:0}} >
+                            <Link onClick={()=>{setPageRoute('')}} href={'/aboutus/aboutus'}>
+                                <ListItem key={'About Us'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'About BNT'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                            <Link onClick={()=>{setPageRoute('')}} href={'/aboutus/lifeatBNT'}>
+                                <ListItem key={'About Us'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'Life at BNT'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+
+                        </List>
+
+                    </>}
+
+
+                    <Link onClick={(e) => {e.stopPropagation(); pageRoute==='/services'? setPageRoute(''): setPageRoute('/services') }} href={''}>
+                        <ListItem  sx={{...(pageRoute ==='/services' &&  {backgroundColor:'#dbdbd9'})}} key={'Services'} disablePadding>
+                            <ListItemButton className={`${router.asPath.includes('/services')? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Services'} /> { pageRoute ==='/services' ? <IoIosArrowDown />: <IoIosArrowForward />}
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+
+                    {pageRoute === '/services' && <>
+                        <List className={styles.mobileNavbarDropdow} sx={{backgroundColor:'#e3f2fc',padding:0}} >
+                            <Link onClick={()=>{setPageRoute('')}} href={'/services/payments'}>
+                                <ListItem key={'Payments'} disablePadding>
+                                    <ListItemButton  sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'Payments'} /> 
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                            <Link onClick={()=>{setPageRoute('')}} href={'/services/retail'}>
+                                <ListItem key={'Retail'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'Retail'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                            <Link onClick={()=>{setPageRoute('')}} href={'/services/fintech'}>
+                                <ListItem key={'Fintech'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'Fintech'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                            <Link onClick={()=>{setPageRoute('')}} href={'/services/subservices'}>
+                                <ListItem key={'Custom'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'Custom'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                            <Link onClick={()=>{setPageRoute('')}} href={'/services/newtechnologies'}>
+                                <ListItem key={'New Technologies'} disablePadding>
+                                    <ListItemButton sx={{ textAlign: 'center' }}>
+                                        <ListItemText primary={'New Technologies'} />
+                                    </ListItemButton>
+                                </ListItem>
+                            </Link>
+                        </List>
+
+                    </>}
+
+                    <Link href={'/solutions'}>
+                        <ListItem key={'Solutions'} disablePadding>
+                            <ListItemButton className={` ${router.asPath == '/solutions' ? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Solutions'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+
+                    <Link href={'/blogs'}>
+                        <ListItem key={'Blogs'} disablePadding>
+                            <ListItemButton className={` ${router.asPath == '/blogs' ? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Blogs'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+
+                    <Link href={'/career'}>
+                        <ListItem key={'Career'} disablePadding>
+                            <ListItemButton className={` ${router.asPath == '/career' ? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Career'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+
+                    <Link href={'/buildwithus'}>
+                        <ListItem key={'Build With Us'} disablePadding>
+                            <ListItemButton className={` ${router.asPath == '/buildwithus' ? styles.navActive : ''}`} sx={{ textAlign: 'center' }}>
+                                <ListItemText primary={'Build With Us'} />
+                            </ListItemButton>
+                        </ListItem>
+                    </Link>
+                </>
+
             </List>
         </Box>
     );
@@ -91,13 +214,13 @@ function Navbar(props) {
             <Box sx={{ display: 'flex', }} >
                 <HideOnScroll {...props} >
                     <AppBar elevation={0} className={'navScroll'} component="nav" sx={{ backgroundColor: 'transparent' }}>
-                        <Toolbar sx={{ minHeight: '55px!important',position:"relative" }}>
+                        <Toolbar sx={{ minHeight: '55px!important', position: "relative" }}>
                             <IconButton
                                 color="inherit"
                                 aria-label="open drawer"
                                 edge="start"
                                 onClick={handleDrawerToggle}
-                                sx={{ mr: 2, display: { sm: 'none' },position:'absolute',right:0 }}
+                                sx={{ mr: 2, display: { sm: 'none' }, position: 'absolute', right: 0 }}
                             >
                                 <MenuIcon />
                             </IconButton>
@@ -112,61 +235,61 @@ function Navbar(props) {
 
                             </Box>
                             <Box sx={{ flexGrow: 1, justifyContent: 'space-between', display: { xs: 'none', sm: 'flex' } }}>
-                                
-                                        <Link href='/'>
-                                            <button
-                                                className={`raise ${router.asPath == '/'? styles.navActive : styles.navList}`}
-                                            >
-                                                Home
-                                            </button>
-                                        </Link>
-                                        <Link  href='/aboutus'>
-                                            <button
-                                                className={`raise ${router.asPath == '/aboutus' ? styles.navActive : styles.navList}`}
-                                                
-                                            >
-                                                About Us
-                                            </button>
-                                        </Link>
-                                        <Link href='services'>
-                                            <button
-                                                className={`raise ${router.asPath == '/services' ? styles.navActive : styles.navList}`}
-                                            >
-                                               Services
-                                            </button>
-                                        </Link>
-                                        <Link href='/solutions'>
-                                            <button
-                                                className={`raise ${router.asPath == '/solutions' ? styles.navActive : styles.navList}`}
-                                            >
-                                                Solutions
-                                            </button>
-                                        </Link>
-                                        <Link href='/blogs'>
-                                            <button
-                                                className={`raise ${router.asPath == '/blogs' ? styles.navActive : styles.navList}`}
-                                            >
-                                                Blogs
-                                            </button>
-                                        </Link>
-                                        <Link href='/career'>
-                                            <button
-                                                className={`raise ${router.asPath == '/career' ? styles.navActive : styles.navList}`}
-                                            >
-                                                Career
-                                            </button>
-                                        </Link>
-                                        <Link href='/buildwithus'>
-                                            <button
-                                                className={`raise ${router.asPath == '/buildwithus' ? styles.navActive : styles.navList}`}
-                                            >
-                                                Build With Us
-                                            </button>
-                                        </Link>
-                                       
+
+                                <Link href='/'>
+                                    <button
+                                        className={`raise ${router.asPath == '/' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Home
+                                    </button>
+                                </Link>
+                                <Link href='/aboutus'>
+                                    <button
+                                        className={`raise ${router.asPath == '/aboutus' ? styles.navActive : styles.navList}`}
+
+                                    >
+                                        About Us
+                                    </button>
+                                </Link>
+                                <Link href='services'>
+                                    <button
+                                        className={`raise ${router.asPath == '/services' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Services
+                                    </button>
+                                </Link>
+                                <Link href='/solutions'>
+                                    <button
+                                        className={`raise ${router.asPath == '/solutions' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Solutions
+                                    </button>
+                                </Link>
+                                <Link href='/blogs'>
+                                    <button
+                                        className={`raise ${router.asPath == '/blogs' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Blogs
+                                    </button>
+                                </Link>
+                                <Link href='/career'>
+                                    <button
+                                        className={`raise ${router.asPath == '/career' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Career
+                                    </button>
+                                </Link>
+                                <Link href='/buildwithus'>
+                                    <button
+                                        className={`raise ${router.asPath == '/buildwithus' ? styles.navActive : styles.navList}`}
+                                    >
+                                        Build With Us
+                                    </button>
+                                </Link>
+
                             </Box>
-                           
-                           
+
+
                             <Box sx={{ flexGrow: 1, }}></Box>
                         </Toolbar>
                     </AppBar>
