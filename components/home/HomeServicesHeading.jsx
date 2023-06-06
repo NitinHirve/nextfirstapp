154b3c8b8a6c8311ba331b9f67ed8229a4b749c3
NitinHirve <hirvernitin@gmail.com>
2023-06-07 02:13:24 +0530

    Added ScrollReveal animation library test component

--- components/home/HomeServicesHeading.jsx
+++ components/home/HomeServicesHeading.jsx
@@ -2,11 +2,25 @@ import { Typography } from '@mui/material'
 import { Box, styled } from '@mui/system'
 import React, { useEffect } from 'react'
 import styles from '../../styles/home/homeservices.module.css'
+import ScrollReveal from 'scrollreveal';
 
 
 
 
 const HomeServicesHeading = () => {
+
+    useEffect(() => {
+        const sr = ScrollReveal();
+    
+        sr.reveal('.my-element', {
+          duration: 1000,
+          distance: '90px',
+          easing: 'ease-in-out',
+          origin: 'bottom',
+          reset: true
+        });
+      }, []);
+
 
 
     return (
@@ -15,16 +29,16 @@ const HomeServicesHeading = () => {
             <img  src="/home/images/svgani/poligon3.svg" style={{ width: '370px',position:'absolute',left:'20px'  }} />
             <Box sx={{ height: {xs:'100px',sm:'180px'}, display: 'flex',justifyContent: 'center'}} >
                 <Box  >
-                    <Box data-aos="fade-up" data-aos-once="false" data-aos-duration="1100" data-aos-offset="100">
+                    <Box className="my-element">
                         <Typography sx={{ fontFamily: 'Alexandria', fontWeight: 300,textAlign:'center', fontSize: { xs: '32px', sm: '110px' }, letterSpacing:{xs:'9px'} , color: '#e8e8e8' }}>SERVICES</Typography>
                     </Box>
-                    <Box data-aos="fade-up" data-aos-once="false" data-aos-duration="1500" data-aos-offset="100">
+                    <Box className="my-element" >
                         <Typography sx={{ fontFamily: 'Alexandria', fontWeight: 300, color: '#1e8be1', textAlign: 'center', fontSize: { xs: '41px', sm: '55px' }, marginTop: { xs: '-33px', sm: '-80px' } }}>What we do?</Typography>
                     </Box>
                 </Box>
             </Box>
             <Box sx={{padding:{xs:'0 20px 30px 20px',sm:"0 160px 50px 160px"}}} >
-                <Typography data-aos="fade-up" data-aos-once="false" sx={{textAlign: 'center', fontFamily: 'Alexandria', fontWeight: '300', color: '#676767', fontSize: '18px' }}>
+                <Typography className="my-element" sx={{textAlign: 'center', fontFamily: 'Alexandria', fontWeight: '300', color: '#676767', fontSize: '18px' }}>
                     Let us know which industry you belong to? With the rapidly growing Fintech market,
                     we at BNT provide the latest and evolved technology know-how, building remarkable payment
                     experiences. Offering dynamic and rigorous payment solutions, we cater to developing a
--- components/home/Services.jsx
+++ components/home/Services.jsx
@@ -1,17 +1,32 @@
 import { Box, Typography,Button } from '@mui/material'
-import React from 'react'
+import React,{useEffect} from 'react'
 import styles from '../../styles/home/services.module.css'
 import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight';
 import Link from 'next/link'
+import ScrollReveal from 'scrollreveal';
+
 
 
 const Services = ({section, title, linkRoute, desc}) => {
+
+    useEffect(() => {
+        const sr = ScrollReveal();
+    
+        sr.reveal('.my-element', {
+          duration: 1000,
+          distance: '90px',
+          easing: 'ease-in-out',
+          origin: 'bottom',
+          reset: true
+        });
+      }, []);
+
 
     return (
         <>
             <Box className={`${styles.box} ${styles[section]}`}>
                 <Box sx={{ display: 'flex', alignItems: 'center', height: '100%',float:{sm:(section==='section2' || section==='section4')?'left':'right', xs:'left'} }}  >
-                    <Box data-aos="zoom-out-up" sx={{width:{sm:'700px',xs:'auto'}, paddingLeft:{sm:(section==='section2' || section==='section4')?'80px':'', xs:'40px'}}} >
+                    <Box className="my-element"  sx={{width:{sm:'700px',xs:'auto'}, paddingLeft:{sm:(section==='section2' || section==='section4')?'80px':'', xs:'40px'}}} >
                         <Typography fontSize= {{sm:'40px',xs:'35px'}} sx={{ fontFamily: 'Alexandria', fontWeight: '500', color:(section==='section3' || section==='section4')?'#fff':'#424242', ...(section==='section1' &&  {color:{xs:'#fff',sm:'#424242'}})}}>{title}</Typography>
                         <Typography fontSize= {{sm:'22px',xs:'18px'}} sx={{ fontFamily: 'Alexandria', fontWeight: '300', color:(section==='section3' || section==='section4')?'#fff':'#212121',...(section==='section1' &&  {color:{xs:'#fff',sm:'#424242'}})  , borderLeft: '3px solid #1e8be1', paddingLeft: '20px',paddingRight: '20px' }}>{desc}</Typography>
                         {/* <button className={`${styles.btn} ${styles.fourth}`}>Know more &nbsp; {  <KeyboardDoubleArrowRightIcon sx={{ fontSize: '16px' }} />}</button> */}
