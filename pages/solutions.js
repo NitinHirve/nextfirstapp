ee01fb49e984ee6b6f1315b03f1d6595e96b66a4
NitinHirve <hirvernitin@gmail.com>
2023-06-20 10:39:03 +0530

    Changed Solutionspage to dynamic to check animations

--- pages/solutions.js
+++ pages/solutions.js
@@ -1,5 +1,9 @@
 import React from 'react'
-import Solutionspage from '../components/solutions/Solutionspage'
+import dynamic from 'next/dynamic'
+// import Solutionspage from '../components/solutions/Solutionspage'
+const Solutionspage = dynamic(() => import('../components/solutions/Solutionspage'), {
+    ssr: false,
+  })
 
 const solutions = () => {
     return (
